35244a0cdd2b624e2af6b07962603a83a05c334e
Hrysia <krisstavkova@gmail.com>
2020-05-06 22:49:17 +0300

    third commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double x = (-3 * Math.PI)/2, y = 0;
+            Console.WriteLine("X\t             \tY\n");
+            Console.WriteLine("                                \n");
+            while (x <= -Math.PI / 2)
+            {
+                y = x * x * Math.Cos(x);
+                Console.WriteLine("x=" + x + "     y=" + y);               
+                x += Math.PI / 10;
+            }
+            Console.ReadKey();
+        }
+    }
+}
